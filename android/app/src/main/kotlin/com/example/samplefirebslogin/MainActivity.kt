dd09e554d28799bfa3370c52fad4f5d67efb4534
VishnuSabu97 <vishnukunnumpurath1@gmail.com>
2023-03-01 11:49:46 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/samplefirebslogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.samplefirebslogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:samplefirebslogin/Authentication.dart';
+import 'package:samplefirebslogin/main.dart';
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Welcome'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          AuthenticationHelper()
+              .signOut()
+              .then((_) => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (contex) => MyAppLogin()),
+          ));
+        },
+        tooltip: 'Logout',
+        child: const Icon(Icons.logout),
+      ),
+    );
+  }
+}
